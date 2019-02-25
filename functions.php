34267f2cab1f9c044ee4c98544c9155998516a06
Renz Ramos <im.renzramos@gmail.com>
2019-02-25 12:24:46 +0800

    Upgraded to Bootstrap 4

--- functions.php
+++ functions.php
@@ -15,10 +15,10 @@ add_action( 'after_setup_theme', 'basic_setup' );
 // enqueue scripts
 function enqueue_scripts() {
 
-	wp_enqueue_style( 'bootstrap', get_stylesheet_directory_uri() . '/assets/css/bootstrap.min.css', array(), null);
+	wp_enqueue_style( 'bootstrap', get_stylesheet_directory_uri() . '/node_modules/dist/css/bootstrap.min.css', array(), null);
     wp_enqueue_style( 'style', get_stylesheet_uri(), array(), null  );
 
-	wp_enqueue_script('bootstrap', get_stylesheet_directory_uri() . '/assets/js/bootstrap.js', array() ,null, true);
+	wp_enqueue_script('bootstrap', get_stylesheet_directory_uri() . '/node_modules/dist/js/bootstrap.js', array() ,null, true);
 	wp_enqueue_script('script', get_stylesheet_directory_uri() . '/assets/js/script.js', array() ,null, true);
 	
 	// Localize the script with new data
