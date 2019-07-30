7c402609a92e74b9fdd2b1ff80092fdcebc6cd03
Renz Ramos <im.renzramos@gmail.com>
2019-07-30 14:26:55 +0800

    Create page-custom-page.php
    Custom page sample

---
+++ page-templates/page-custom-page.php
@@ -0,0 +1,11 @@
+<?php /* Template Name: Custom Page */
+get_header(); ?>
+
+    <!-- main content -->
+    <div class="main-content">
+
+    </div>
+    <!-- .main content -->
+
+<?php get_footer(); ?>
+
